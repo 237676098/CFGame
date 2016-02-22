be19a146d3c8f6523b6c19cd4514494088cf0a29
zhouxu <237676098@qq.com>
2016-02-23 00:16:39 +0800

    相机类

--- CFGame/CFGame/base/Camera.cpp
+++ CFGame/CFGame/base/Camera.cpp
@@ -29,3 +29,109 @@ void Camera::SetLens(float fov, float aspect, float nearz, float farz)
 	XMStoreFloat4x4(&mProj, P);
 }
 
+XMVECTOR Camera::GetPositionXM() const
+{
+	return XMLoadFloat3(&mPosition);
+}
+
+XMFLOAT3 Camera::GetPosition() const
+{
+	return mPosition;
+}
+
+XMVECTOR Camera::GetRightXM() const
+{
+	return XMLoadFloat3(&mRight);
+}
+
+XMFLOAT3 Camera::GetRight() const
+{
+	return mRight;
+}
+
+XMVECTOR Camera::GetUpXM()const
+{
+	return XMLoadFloat3(&mUp);
+}
+
+XMFLOAT3 Camera::GetUp() const
+{
+	return mUp;
+}
+
+XMVECTOR Camera::GetLookXM() const
+{
+	return XMLoadFloat3(&mLook);
+}
+
+XMFLOAT3 Camera::GetLook() const
+{
+	return mLook;
+}
+
+float Camera::GetNearZ()const
+{
+	return mNearZ;
+}
+
+float Camera::GetFarZ()const
+{
+	return mFarZ;
+}
+
+float Camera::GetAspect()const
+{
+	return mAspect;
+}
+
+float Camera::GetFovY()const
+{
+	return mFovY;
+}
+
+float Camera::GetFovX()const
+{
+	float halfWidth = 0.5f*GetNearWindowWidth();
+	return 2.0f*atan(halfWidth / mNearZ);
+}
+
+float Camera::GetNearWindowWidth()const
+{
+	return mAspect * mNearWindowHeight;
+}
+
+float Camera::GetNearWindowHeight()const
+{
+	return mNearWindowHeight;
+}
+
+float Camera::GetFarWindowWidth()const
+{
+	return mAspect * mFarWindowHeight;
+}
+
+float Camera::GetFarWindowHeight()const
+{
+	return mFarWindowHeight;
+}
+
+void Camera::LookAt(FXMVECTOR pos, FXMVECTOR target, FXMVECTOR worldUp)
+{
+	XMVECTOR L = XMVector4Normalize(XMVectorSubtract(target, pos));
+	XMVECTOR R = XMVector4Normalize(XMVector3Cross(L, worldUp));
+	XMVECTOR U = XMVector3Cross(L, R);
+
+	XMStoreFloat3(&mPosition, pos);
+	XMStoreFloat3(&mLook, L);
+	XMStoreFloat3(&mRight, R);
+	XMStoreFloat3(&mUp, U);
+}
+
+void Camera::LookAt(const XMFLOAT3 &pos, const XMFLOAT3 &target, const XMFLOAT3 &up)
+{
+	XMVECTOR P = XMLoadFloat3(&pos);
+	XMVECTOR T = XMLoadFloat3(&target);
+	XMVECTOR U = XMLoadFloat3(&up);
+
+	LookAt(P, T, U);
+}
